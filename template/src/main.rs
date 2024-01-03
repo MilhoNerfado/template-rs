2d28b0f787dd67f3e4d825f633c8a616b9a8c116
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-01-03 16:03:42 -0300

    fix: Fix main for a "simple" hello world

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,7 +1,5 @@
-
 #[tokio::main]
 async fn main() {
-    println!("Example bool: {}", CONFIG.example_bool);
-    println!("Example list: {:?}", CONFIG.example_list);
+    println!("Hello World!");
 }
 
